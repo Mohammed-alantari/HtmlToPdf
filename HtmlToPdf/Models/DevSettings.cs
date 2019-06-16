2d8024239b141a243f98ab7d3dc4c469554c3f78
Mohammed-alantari <mohammed.ali.alantari@gmail.com>
2019-06-17 01:12:25 +0300

    add project file and prepare solution

---
+++ HtmlToPdf/Models/DevSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf.Models
+{
+    public class DevSettings
+    {
+
+        private static DevName name = DevName.Mohammed;
+        private static DevDb db = DevDb.local;
+
+        public static string DevConnectionStrings()
+        {
+            switch (db)
+            {
+                case DevDb.Hobbyst:
+                    return "Data Source=tcp:hobbystsqlserver.database.windows.net,1433;Initial Catalog=HobbystDB;User ID=HobbystAdmin;Password=1223!@@#qwweQWWE";
+                case DevDb.local:
+                    return "Server=(localdb)\\mssqllocaldb;Database=HobbystDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+                default:
+                    return "";
+            }
+        }
+    }
+    public enum DevName
+    {
+        Mohammed
+    }
+    public enum DevDb
+    {
+        Hobbyst,
+        local
+    }
+
+
+}
+
--- HtmlToPdf/Startup.cs
+++ HtmlToPdf/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,7 +33,11 @@ namespace HtmlToPdf
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DbCon>(options =>
+               options.UseSqlServer(Models.DevSettings.DevConnectionStrings()));
 
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<DbCon>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
